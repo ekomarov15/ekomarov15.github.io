80cd070eee74c235f07f602dc02022bb5dd20d56
ekomarov15 <ekomarov_2010@bk.ru>
2026-01-21 00:25:38 +0300

    Create oge-structure.js

---
+++ oge-structure.js
@@ -0,0 +1,15 @@
+const OGE_STRUCTURE = {
+  1: { type: "summary", max: 7 },
+  2: { type: "choice", max: 1 },
+  3: { type: "choice", max: 1 },
+  4: { type: "choice", max: 1 },
+  5: { type: "choice", max: 1 },
+  6: { type: "choice", max: 1 },
+  7: { type: "choice", max: 1 },
+  8: { type: "choice", max: 1 },
+  9: { type: "choice", max: 1 },
+  10:{ type: "choice", max: 1 },
+  11:{ type: "choice", max: 1 },
+  12:{ type: "choice", max: 1 },
+  13:{ type: "essay", max: 9 }
+};
