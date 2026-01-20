013ca632016fb3dd3da4ce9e42534eed71a3b859
ekomarov15 <ekomarov_2010@bk.ru>
2026-01-21 01:04:07 +0300

    Update oge-structure.js

--- js/data/oge-structure.js
+++ js/data/oge-structure.js
@@ -1,6 +1,15 @@
 const OGE_STRUCTURE = {
-  1: { name: "Сжатое изложение", type: "summary" },
-  2: { name: "Грамматическая основа", type: "test" },
+  1: { name: "Сжатое изложение", type: "text" },
+  2: { name: "Орфография", type: "test" },
   3: { name: "Пунктуация", type: "test" },
+  4: { name: "Синтаксис", type: "test" },
+  5: { name: "Лексика", type: "test" },
+  6: { name: "Средства выразительности", type: "test" },
+  7: { name: "Тестовое задание", type: "test" },
+  8: { name: "Тестовое задание", type: "test" },
+  9: { name: "Тестовое задание", type: "test" },
+  10:{ name: "Тестовое задание", type: "test" },
+  11:{ name: "Тестовое задание", type: "test" },
+  12:{ name: "Тестовое задание", type: "test" },
   13:{ name: "Сочинение", type: "essay" }
 };
