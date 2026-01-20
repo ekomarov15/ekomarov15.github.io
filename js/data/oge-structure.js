4df14cfd85258b553a803e005323318e9e09980f
ekomarov15 <ekomarov_2010@bk.ru>
2026-01-21 00:57:35 +0300

    Create oge-structure.js

---
+++ js/data/oge-structure.js
@@ -0,0 +1,6 @@
+const OGE_STRUCTURE = {
+  1: { name: "Сжатое изложение", type: "summary" },
+  2: { name: "Грамматическая основа", type: "test" },
+  3: { name: "Пунктуация", type: "test" },
+  13:{ name: "Сочинение", type: "essay" }
+};
