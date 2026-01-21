4bb5a7966b3be3b40b18062c963a17f95c1d0fd6
ekomarov15 <ekomarov_2010@bk.ru>
2026-01-21 14:30:43 +0300

    Delete backend directory

--- backend/generateVariant.js
+++
@@ -1,8 +0,0 @@
-const tasks = []
-
-for (let i = 1; i <= 27; i++) {
-  const num = String(i).padStart(2, '0')
-  tasks.push(require(`./generators/task${num}`))
-}
-
-module.exports = () => tasks.map(t => t())
